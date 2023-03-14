76985d00bf4ee6c79fb0dc8a6f78c27128498166
Nikhil M <nrmeria@gmail.com>
2023-03-14 20:54:50 +0530

    basic design for index page done

---
+++ pages/chat.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+function Chat() {
+ return (
+  <>
+   <div>
+    <Link href="/">
+     <p>back</p>
+    </Link>
+    <h2>Chat -----</h2>
+   </div>
+  </>
+ );
+}
+
+export default Chat;
