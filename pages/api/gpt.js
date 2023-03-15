fb2fb6c58ae612aaf9ad5d663af832973833ec42
Nikhil M <nrmeria@gmail.com>
2023-03-15 19:13:40 +0530

    all basic functionality done

---
+++ pages/api/gpt.js
@@ -0,0 +1,25 @@
+import {Configuration, OpenAIApi} from "openai";
+
+const configuration = new Configuration({
+ apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  const prompt = req.query.prompt;
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Answer to my prompt.\n
+    Topic: ${prompt}\n`,
+    max_tokens: 500,
+    temperature: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+  });
+
+  const quote = completion.data.choices[0].text;
+
+  res.status(200).json({ quote });
+}
--- pages/chat.js
+++ pages/chat.js
@@ -1,15 +1,43 @@
-import {FormEvent, useState} from 'react'
+import { useState} from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import chatStyle from "../styles/Chat.module.css"
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Spinner, Toast, ToastContainer } from "react-bootstrap";
 
 function Chat() {
- 
+ const[gptResp, setGptResp]= useState("");
+ const [isLoading, setIsLoading] = useState(false);
+ const [onErr, setOnErr] = useState(false);
 
  const handleSubmit= async(e)=>{
    e.preventDefault();
-   console.log("Button CLicked");
+
+   console.log("Button CLicked : ", e.target);
+
+   const formData = new FormData(e.target);
+   const prompt= formData.get("prompt")?.toString().trim();
+
+   console.log("Prompt : ", prompt);
+
+   if(prompt){
+    try {
+     setOnErr(false);
+     setIsLoading(true);
+     setGptResp("");
+
+     const response = await fetch(
+      "/api/gpt?prompt=" + encodeURIComponent(prompt)
+     );
+     const body = await response.json();
+     setGptResp(body.quote);
+    } catch (error) {
+     console.error(error);
+     setOnErr(true);
+    } finally {
+     setIsLoading(false);
+    }
+   }
+   
  }
 
  return (
@@ -31,10 +59,19 @@ function Chat() {
       <Form.Label className="mt-1">What's your ques?</Form.Label>
       <Form.Control name="prompt" placeholder="type here" maxLength={150} />
      </Form.Group>
-     <Button type="submit" className="mt-3">
+     <Button type="submit" className="mt-3" disabled={isLoading}>
       Submit
      </Button>
     </Form>
+    {isLoading && <Spinner className="mt-3" animation="border"></Spinner>}
+    {onErr && (
+     <ToastContainer position="top-end" className="me-3 mt-3">
+      <Toast autohide delay={3000} bg={"danger"}>
+       <Toast.Body color='white'>Something went wrong, try again.</Toast.Body>
+      </Toast>
+     </ToastContainer>
+    )}
+    {gptResp && <h5 className="h5">{gptResp}</h5>}
    </div>
   </>
  );
