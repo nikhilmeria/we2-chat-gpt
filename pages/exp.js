834d0094304399682ccf5558c6cc75ed16364e21
Nikhil M <nrmeria@gmail.com>
2023-03-17 19:55:03 +0530

    added home button on all non working pages

--- pages/exp.js
+++ pages/exp.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import chatStyle from "../styles/Example.module.css";
 
 function Example() {
  return (
   <>
    <div className={chatStyle.exp_container}>
+    <Link href="/" style={{textDecoration: "none", textAlign: "center"}}>
+     <h6 className="h3">&larr; Home</h6>
+    </Link>
     <Image
      src={"/images/gitaImage.png"}
      width={550}
--- pages/gita.js
+++ pages/gita.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import chatStyle from "../styles/Gita.module.css";
 
 function Gita() {
  return (
   <>
    <div className={chatStyle.gita_container}>
+    <Link href="/" style={{textDecoration: "none", textAlign:"center"}}>
+     <h6 className="h3">&larr; Home</h6>
+    </Link>
     <Image
      src={"/images/gitaImage.png"}
      width={550}
